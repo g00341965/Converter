f18416e827430f14ff00559ff1d60ed07c7f0de7
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-02-14 14:50:40 +0000

    Add files via upload

--- converter.js
+++ converter.js
@@ -9,3 +9,16 @@ function cToF(){
 	document.getElementById("resultsentence").innerHTML = message;
 	
 }
+
+function fToC(){
+	
+	var fTemp = parseInt(document.getElementById("value1").value);
+	
+	var fToC = (fTemp-32) * 5 / 9;
+	
+	var message = fTemp+'\xB0F is ' + fToC + ' \xB0C.'; 
+	console.log(message);
+	document.getElementById("resultsentence").innerHTML = message;
+	
+}
+
